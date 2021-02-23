c670c5bc1554586800d7e943f011998ff8b181b4
fepu08 <ferencbesenyei@protonmail.com>
2021-02-23 19:54:00 +0100

    add homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { prefix } from '../../config';
+import title from '../../img/title.png';
+import {  connect } from 'react-redux';
+import PropTypes from 'prop-types'
 
 const Home = () => {
-  return (
-    <div>
-      <div className='menu d-flex flex-column justify-content-center align-items-center'>
-            <Link to={`${prefix}/quiz`} className='btn btn-primary btn-block btn-menu'>
-              Start Quiz
-            </Link>
-            <Link to={`${prefix}/questions`} className='btn btn-primary btn-block btn-menu'>
-              Edit Questions
-            </Link>
-            <Link to={`${prefix}/leaderboard`} className='btn btn-primary btn-block btn-menu'>
-              Leader Board
-            </Link>
-          </div>
-    </div>
-  )
-}
+  useEffect(() => {
+    document.body.id = 'gb1';
+    //eslint-disable-next-line
+  }, []);
 
-export default Home
+  return (
+    <div className='menu d-flex flex-column justify-content-center align-items-center'>
+      <div id='title-img-container'>
+        <img src={title} alt='I am Smarticus' />
+      </div>
+      <Link
+        to={`${prefix}/quiz`}
+        className='btn btn-primary btn-block btn-menu'
+      >
+        Start Quiz
+      </Link>
+      <Link
+        to={`${prefix}/questions`}
+        className='btn btn-secondary btn-block btn-menu'
+      >
+        Edit Questions
+      </Link>
+      <Link
+        to={`${prefix}/leaderboard`}
+        className='btn btn-danger btn-block btn-menu'
+      >
+        Leader Board
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
