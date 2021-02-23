6d04a0f421d96f768c68ec82ee634647dd584043
fepu08 <ferencbesenyei@protonmail.com>
2021-02-23 09:42:27 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { prefix } from './config';
+import { Container, Row } from 'react-bootstrap';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <h1 className='text-primary'>Hello World!</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path={`${prefix}/`} />
+          <Route exact path={`${prefix}/quiz`} />
+          <Route exact path={`${prefix}/leaderboard`} />
+          <Route exact path={`${prefix}/questions`} />
+        </Switch>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
