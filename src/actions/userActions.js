434f0898bee75ab078a3e88188b356baf48dfaf1
fepu08 <ferencbesenyei@protonmail.com>
2021-02-26 17:30:38 +0100

    user actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,4 +1,10 @@
-import { SET_NAME, SET_SCORE, CLEAR_USER } from './types';
+import {
+  SET_NAME,
+  SET_SCORE,
+  CLEAR_USER,
+  SET_CURRENT_ANSWER,
+  CLEAR_CURRENT_ANSWER
+} from './types';
 
 export const setName = (name) => {
   return {
@@ -11,6 +17,19 @@ export const setScore = (score) => {
   return {
     type: SET_SCORE,
     payload: score
+  };
+};
+
+export const setCurrentAnswer = (answer) => {
+  return {
+    type: SET_CURRENT_ANSWER,
+    payload: answer
+  };
+};
+
+export const clearCurrentAnswer = () => {
+  return {
+    type: CLEAR_CURRENT_ANSWER
   };
 };
 
