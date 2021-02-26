f95423d53eaf06723c3ddde4c7e18db79a35f9ef
fepu08 <ferencbesenyei@protonmail.com>
2021-02-26 21:48:44 +0100

    update alertActions to auto remove alerts

--- src/actions/alertActions.js
+++ src/actions/alertActions.js
@@ -1,9 +1,10 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
+import { v4 as uuidv4 } from 'uuid';
 
 export const setAlert = (alert) => {
   return {
     type: SET_ALERT,
-    payload: alert
+    payload: { id: uuidv4(), ...alert }
   };
 };
 
