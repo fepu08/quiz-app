ef6b20412d52ef9b1280307ba2ad172bb2f06d5e
fepu08 <ferencbesenyei@protonmail.com>
2021-02-26 17:35:00 +0100

    add private routing to quiz and add quiz menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PrivateRoute from './components/routing/PrivateRoute';
 import { prefix } from './config';
 import { Container, Row } from 'react-bootstrap';
 
@@ -8,6 +9,7 @@ import Home from './components/pages/Home';
 import NameForm from './components/pages/NameForm';
 import Leaderboard from './components/leaderboard/Leaderboard';
 import Questions from './components/questions/Questions';
+import QuizMenu from './components/quiz/QuizMenu';
 
 const App = () => {
   return (
@@ -16,6 +18,7 @@ const App = () => {
         <Switch>
           <Route exact path={`${prefix}/`} component={Home} />
           <Route exact path={`${prefix}/name`} component={NameForm} />
+          <PrivateRoute exact path={`${prefix}/quiz`} component={QuizMenu} />
           <Route exact path={`${prefix}/leaderboard`} component={Leaderboard} />
           <Route exact path={`${prefix}/questions`} component={Questions} />
         </Switch>
