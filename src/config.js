31ac0f718443d33424303e51fab045363382a3f6
fepu08 <ferencbesenyei@protonmail.com>
2021-02-26 17:33:54 +0100

    updated config

--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ export const alertTimeout = 5000;
 // export const my_json_server =
 //   'https://my-json-server.typicode.com/fepu08/quiz-app';
 export const my_json_server = '';
+export const points_for_correct_answers = 100;
