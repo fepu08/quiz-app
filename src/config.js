cab96e1d4192dea8bf6b12f8a02cd54a09409569
fepu08 <ferencbesenyei@protonmail.com>
2021-02-26 18:29:31 +0100

    update gitignore

--- src/config.js
+++
@@ -1,7 +0,0 @@
-// export const prefix = '';
-export const prefix = '/quiz-app';
-export const alertTimeout = 5000;
-export const my_json_server =
-  'https://my-json-server.typicode.com/fepu08/quiz-app';
-// export const my_json_server = '';
-export const points_for_correct_answers = 100;
